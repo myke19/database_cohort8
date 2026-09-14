2469b8e8cc918872a4d1404b8cb20c254aff47c8
myke19 <apeyiomotobi04@gmail.com>
2026-09-14 20:54:36 +0100

    loginUser

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,7 @@ userRoute.get("/all-users", getAllUsers)
 userRoute.get("/get-one-user/:id", getSingleUser)
 userRoute.delete("/delete-user/:userId", deleteUser)
 userRoute.patch("/update-user/:userId", updateUser)
-userRoute.get("/login", longinUser)
+userRoute.get("/login", loginUser)
 
 
 module.exports = userRoute
